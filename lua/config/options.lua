2278f8fe26b7cbada52f06924419121360bbaad2
Madiba Elvis <diba@Madibas-MacBook-Pro.local>
2025-08-02 20:34:51 +0300

    feat: Wrap lines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
+--
+vim.opt.wrap = true -- Wrap lines instead of scrolling horizontally
+vim.opt.linebreak = true -- Wrap at word boundaries (don't break mid-word)
+vim.opt.breakindent = true -- Preserve indentation on wrapped lines
+vim.opt.showbreak = "↪ "
