c97bcf626e27e134e64c131ff78611f3cb9dfe93
Madiba Elvis <diba@Madibas-MacBook-Pro.local>
2025-10-14 14:16:06 +0300

    chore: Lazyvim update fixes

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,6 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
