923f0afd70915bc91c9daaf64095ada64c9a7b62
not-diba <elvis.madiba@gmail.com>
2025-10-31 14:47:18 +0300

    feat: Nvim tmux navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,4 @@
--- Preview image keymap
-vim.keymap.set("n", "<leader>pi", function()
-  require("image_preview").PreviewImage(vim.fn.expand("%:p"))
-end, { desc = "󰸵 Preview image" })
-
--- Which-key registration (add this to your which-key config)
-return {
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.spec = opts.spec or {}
-      vim.list_extend(opts.spec, {
-        { "<leader>p", group = "Preview", icon = "󰸵" },
-        { "<leader>pi", desc = "Image", icon = "󰸵" },
-      })
-    end,
-  },
-}
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
