00afeb0c92cd59b68cbf12eefdaa96dcba5aaf6c
not-diba <elvis.madiba@gmail.com>
2025-11-06 18:15:03 +0300

    fix: mismatched which key config

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -42,8 +42,8 @@ return {
       { "<leader>rt", ":RunFile tab<CR>", desc = "Run File in Tab" },
       { "<leader>rp", ":RunProject<CR>", desc = "Run Project" },
       { "<leader>rc", ":RunClose<CR>", desc = "Close Runner" },
-      { "<leader>crf", ":CRFiletype<CR>", desc = "Set Filetype Runner" },
-      { "<leader>crp", ":CRProjects<CR>", desc = "Set Project Runner" },
+      { "<leader>rt", ":CRFiletype<CR>", desc = "Set Filetype Runner" },
+      { "<leader>rs", ":CRProjects<CR>", desc = "Set Project Runner" },
     },
   },
 
@@ -54,7 +54,6 @@ return {
       opts.spec = opts.spec or {}
       vim.list_extend(opts.spec, {
         { "<leader>r", group = "Run", icon = "" },
-        { "<leader>cr", group = "CodeRunner Config", icon = "" },
       })
     end,
   },
